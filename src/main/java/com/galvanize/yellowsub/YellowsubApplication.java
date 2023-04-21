83decdfe68cb2f475197025bef357ac82409e66e
Russhi Gibbs <ladyg21218@gmail.com>
2023-04-21 10:25:07 -0500

    initial commit yellow sub

---
+++ src/main/java/com/galvanize/yellowsub/YellowsubApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.yellowsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YellowsubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/yellowsub/YellowsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.yellowsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YellowsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
